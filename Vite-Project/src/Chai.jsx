94c61cd7f7e4e3015f9be23ced108ff494a89764
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-11-08 05:42:36 +0000

    Rea

---
+++ Vite-Project/src/Chai.jsx
@@ -0,0 +1,4 @@
+function Chai(){
+    return <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam sapiente ut veniam enim, numquam quasi quaerat eligendi, debitis optio dolorem, rem ea aliquid sunt possimus tenetur voluptatibus nostrum rerum?</p>
+}
+export default Chai;
